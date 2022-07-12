4a06e394f2fb4346d82a18fd6279dbdc1dd8e11b
FelipeHiroshi13 <felipehiroshi13@gmail.com>
2022-07-11 22:27:07 -0400

    CRUD and Paginator

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('helloword/', views.helloWord),
     path('', views.taskList, name='task-list'),
-    path('yourname/<str:name>', views.yourName, name='your-name')
+    path('yourname/<str:name>', views.yourName, name='your-name'),
+    path('task/<int:id>', views.taskView, name="task-view"),
+    path('edit/<int:id>', views.editTask, name="edit-task"),
+    path('newtask/', views.newTask, name="new-task"),
+    path('delete/<int:id>', views.deleteTask, name="delete-task"),
 ]
