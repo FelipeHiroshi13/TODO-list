9099f50421b532ce0cd30f01e711ca58dfb69965
FelipeHiroshi13 <felipehiroshi13@gmail.com>
2022-07-14 10:34:04 -0400

    login and logout

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,28 +1,39 @@
 from django.shortcuts import get_object_or_404, redirect, render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Task
 from .forms import TaskForm
 from django.core.paginator import Paginator 
 from django.http import HttpResponse
 from django.contrib import messages
 
+
+@login_required
 def taskList(request):
-    tasks_list = Task.objects.all().order_by('-created_at')
     
-    paginator = Paginator(tasks_list, 3)
-    
-    page = request.GET.get('page')
-    
-    tasks = paginator.get_page(page)
+    search = request.GET.get('search') 
+
+    if search:
+
+        tasks = Task.objects.filter(title__icontains=search)
+
+    else:
+
+        tasks_list = Task.objects.all().order_by('-created_at')
+        
+        paginator = Paginator(tasks_list, 3)
+        
+        page = request.GET.get('page')
+        
+        tasks = paginator.get_page(page)
     
     return render(request, 'tasks/list.html', {'tasks': tasks})
 
+@login_required
 def taskView(request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'tasks/task.html', {'task': task})
 
-def helloWord(request):
-    return HttpResponse('Hello Word');
-
+@login_required
 def newTask(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
@@ -37,6 +48,7 @@ def newTask(request):
         form = TaskForm()
         return render(request, 'tasks/addtask.html', {'form': form})
 
+@login_required
 def editTask(request, id):
     task = get_object_or_404(Task, pk=id)
     form = TaskForm(instance=task)
@@ -52,7 +64,8 @@ def editTask(request, id):
         
     else:
         return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
-    
+
+@login_required   
 def deleteTask(request, id):
     task = get_object_or_404(Task, pk=id)
     task.delete()
--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tasks',
     'about',
-    'users'
+    'users',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,7 +122,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/accounts/login'
